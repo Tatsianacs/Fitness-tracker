79f7c5974fc5a393e47edbbf10d24fc4931129ed
tania <strekasia@tut.by>
2019-01-14 16:37:36 +0300

    feat: add pwd and email

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule],
-    exports: [MatButtonModule, MatIconModule]
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
 })
 export class MaterialModule {
 }
