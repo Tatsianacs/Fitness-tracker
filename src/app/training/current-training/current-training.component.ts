978139d691591efb1380c5216de46c7ae3ec4ff6
tania <strekasia@tut.by>
2019-01-17 23:55:38 +0300

    feat: add timer

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {MatDialog} from '@angular/material';
 import {StopTrainingComponent} from './stop-training.component';
 
@@ -8,6 +8,7 @@ import {StopTrainingComponent} from './stop-training.component';
     styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+    @Output() trainingExit = new EventEmitter<void>();
     progress = 0;
     timer: number;
     buttonState = 'Stop';
@@ -18,6 +19,10 @@ export class CurrentTrainingComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.startOrResume();
+    }
+
+    startOrResume() {
         this.timer = setInterval(() => {
             this.progress = this.progress + 1;
             if (this.progress >= 100) {
@@ -29,9 +34,13 @@ export class CurrentTrainingComponent implements OnInit {
 
     onClick() {
         clearInterval(this.timer);
-        const dialogRef = this.dialog.open(StopTrainingComponent, {data: {progress:  this.progress}});
+        const dialogRef = this.dialog.open(StopTrainingComponent, {data: {progress: this.progress}});
         dialogRef.afterClosed().subscribe(result => {
-            console.log(result);
+            if (result) {
+                this.trainingExit.emit();
+            } else {
+                this.startOrResume();
+            }
         });
     }
 
